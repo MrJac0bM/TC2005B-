3070e7958698634b6c0eab1a3e9e42e996cc6705
Jacob Espejel Guiza <A01712021@tec.mx>
2025-02-17 19:16:33 -0600

    Realización de Validador de Usuario y contraseña

---
+++ script_lab6.js
@@ -0,0 +1,35 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+const loginForm = document.getElementById('login-form');
+const errorMessage = document.getElementById('error-message');
+
+// Credenciales válidas
+const validUser = 'NEXO@gmail.com';
+const validPassword = 'CreandoElMañana';
+
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+
+
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if (username === validUser && password === validPassword) {
+        errorMessage.style.display = 'none'; 
+        alert('¡Inicio de sesión exitoso!');
+    } else {
+        errorMessage.style.display = 'block'; 
+    }
+});
